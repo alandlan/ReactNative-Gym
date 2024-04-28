77fdd02fbe44fd8b9388780b3b91a9a1450e4571
alandlan <alanhorcrux@gmail.com>
2024-04-28 06:04:54 -0300

    install bottom tabs

---
+++ gym/src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+
+export function Home() {
+    return (
+        <View style={{justifyContent:'center', alignItems:'center', flex:1}}>
+            <Text>Home</Text>
+        </View>
+    );
+}
