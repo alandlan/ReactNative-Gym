7285dda6407d626022961d1d8de99419250abc0b
alandlan <alanhorcrux@gmail.com>
2024-05-04 18:40:50 -0300

    screen signin

---
+++ gym-gluestack/src/components/Input.tsx
@@ -0,0 +1,54 @@
+import { Input as GluestackInput,InputField, InputIcon, InputSlot, MailIcon } from '@gluestack-ui/themed';
+
+import React, { useState } from 'react';
+import { KeyboardType, TextInputProps } from 'react-native';
+
+
+//create inputprops extending textinputprops
+export interface InputProps extends TextInputProps{
+    placeholder: string;
+    icon: any;
+    keyboardType?: KeyboardType;
+}
+
+
+export function Input({placeholder,icon,keyboardType,...rest} : InputProps){
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
+
+    const borderColor = isFocused ? '$green800' : '$trueGray700';
+
+    return (
+        <GluestackInput
+              bg='$trueGray700'
+              h={52}
+              px={16}
+              mb={16}
+              borderColor={borderColor}
+            >
+              <InputSlot>
+                  <InputIcon as={icon} />
+              </InputSlot>
+            <InputField
+                placeholder={placeholder}
+                placeholderTextColor="$trueGray400"
+                color="$white"
+                fontSize="$md"
+                fontFamily='$body'
+                h={52}
+                // borderColor='$green800'
+                keyboardType={keyboardType ? keyboardType : 'default'}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                {...rest}
+            />
+        </GluestackInput>
+    )
+}
--- gym-gluestack/src/screens/SignIn.tsx
+++ gym-gluestack/src/screens/SignIn.tsx
@@ -1,29 +1,58 @@
-import { VStack,ImageBackground, Text, Center } from "@gluestack-ui/themed";
+import { 
+  Input as GluestackInput,
+  VStack,
+  ImageBackground, 
+  Text, 
+  Center, 
+  Heading, 
+  View, 
+  InputSlot, 
+  InputIcon,
+  MailIcon, 
+  EyeOffIcon } from "@gluestack-ui/themed";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImage from "@assets/background.png";
+import { Input } from "@components/Input";
+import { Dimensions } from "react-native";
 
 export function SignIn() {
+  const { width, height } = Dimensions.get('window');
+
   return (
-    <VStack flex={1} bg="$trueGray700" position="relative">
+    <VStack bg="$trueGray800" flex={1}>
         <ImageBackground 
-            source={BackgroundImage} 
-            defaultSource={BackgroundImage}
-            alt="Background"
-            resizeMode="cover"
-            position="absolute"
-            w={"100%"}    
-            h={"100%"}
-        />
+              source={BackgroundImage} 
+              defaultSource={BackgroundImage}
+              alt="Background"
+              resizeMode="cover"
+              position="absolute"
+              w={width}
+              h={height}
+              top={0}
+            />
+        
+          <View px={40}>
+            <Center my={96}>
+              <LogoSvg />
 
-        <Center my={96}>
-          <LogoSvg />
+              <Text color="$trueGray100" fontSize={"$sm"}>
+                  Treine sua mente e o seu corpo
+              </Text>
+            </Center>
 
-          <Text>
-              Treine sua mente e o seu corpo
-          </Text>
-        </Center>
+            <Center>
+              <Heading color="$trueGray100" fontSize={"$xl"} mb={24}>
+                Acesse sua conta
+              </Heading>
+            </Center>
 
+  
+            <Input placeholder="E-mail" keyboardType="email-address" icon={MailIcon}/>
+            
+            <Input placeholder="Senha" secureTextEntry icon={EyeOffIcon}/>
+          </View>
+          
         
         
     </VStack>
