d3a73e124cc7817f43ecaac06b15be1bbba04e9e
alandlan <alanhorcrux@gmail.com>
2024-05-11 17:51:11 -0300

    create header

--- gym-gluestack/src/components/HomeHeader.tsx
+++ gym-gluestack/src/components/HomeHeader.tsx
@@ -1,16 +1,22 @@
 import { HStack, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 
 export function HomeHeader() {
   return (
     <HStack bg="$trueGray600" pt={56} pb={8} px={32} alignItems="center">
+        <UserPhoto 
+            size={48} 
+            source={{uri: "https://avatars.githubusercontent.com/u/20859616?s=400&v=4"}} 
+            alt="userPhoto"
+            style={{marginRight: 8}}/>
         <VStack>
         <Text color="$trueGray100" fontSize={"$md"}>
             Ola
         </Text>
 
         <Heading color="$trueGray100" fontSize={"$md"}>
-            Home
+            Alan Martins
         </Heading>
         </VStack>
     </HStack>
---
+++ gym-gluestack/src/components/UserPhoto.tsx
@@ -0,0 +1,19 @@
+import { Image } from '@gluestack-ui/themed';
+import { ImageProps } from 'react-native';
+
+type Props = ImageProps & {
+    size: number;
+}
+
+export function UserPhoto({size, ...props}: Props){
+    return (
+        <Image 
+            w={size}
+            h={size}
+            rounded={"$full"}
+            borderWidth={2}
+            borderColor={"$trueGray400"}
+            {...props}
+            />
+    )
+}
