01d461154c8b9efa2e4b700cecab9214b01b974a
alandlan <alanhorcrux@gmail.com>
2024-06-25 17:04:13 -0300

    refactor: Update Routes component to use useAuth hook

--- gym-gluestack/src/routes/index.tsx
+++ gym-gluestack/src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { NavigationContainer,DefaultTheme } from "@react-navigation/native";
 import { Box } from "@gluestack-ui/themed";
-import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
-import { useContext } from "react";
-import { AuthContext } from "@contexts/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
 export function Routes(){
-    const contextData = useContext(AuthContext);
+    const {user } = useAuth();
 
     const theme = DefaultTheme;
     theme.colors.background = "$gray700";
