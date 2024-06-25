c6fa435d32c7788fa60ea20f1ff00b3d980e7984
alandlan <alanhorcrux@gmail.com>
2024-06-25 17:16:25 -0300

    chore: Refactor SignIn component to use react-hook-form for form validation and submission

--- gym-gluestack/src/screens/SignIn.tsx
+++ gym-gluestack/src/screens/SignIn.tsx
@@ -16,15 +16,27 @@ import { Dimensions,Image } from "react-native";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { Controller, useForm } from "react-hook-form";
+
+type FormData = {
+  email: string;
+  password: string;
+}
 
 export function SignIn() {
   const { width,height } = Dimensions.get('window');
   const imageHeight = Image.resolveAssetSource(BackgroundImage).height;
 
+  const { control, handleSubmit,formState: {errors} } = useForm<FormData>();
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
   function handleNavigateToSignUp(){
     navigation.navigate("SignUp");
+  }
+
+  function handleSignIn({email,password}: FormData){
+    console.log(email,password);
   }
 
   return (
@@ -57,11 +69,39 @@ export function SignIn() {
               </Center>
 
               <Center>
-                <Input placeholder="E-mail" keyboardType="email-address" autoCapitalize="none" icon={MailIcon}/>
-                
-                <Input placeholder="Senha" secureTextEntry icon={EyeOffIcon}/>
+                <Controller 
+                  control={control}
+                  render={({field:{onChange,value}}) => (
+                    <Input 
+                      placeholder="E-mail" 
+                      keyboardType="email-address" 
+                      autoCapitalize="none" 
+                      icon={MailIcon}
+                      onChangeText={onChange}
+                      value={value}
+                      errorMessage={errors.email?.message}
+                    />
+                  )}
+                  name="email"
+                  rules={{required: 'E-mail é obrigatório'}}
+                />
 
-                <Button title="Acessar" variant="primary" />
+                <Controller 
+                  control={control}
+                  render={({field:{onChange,value}}) => (
+                    <Input 
+                      placeholder="Senha" 
+                      secureTextEntry 
+                      icon={EyeOffIcon}
+                      onChangeText={onChange}
+                      value={value}
+                      errorMessage={errors.password?.message}
+                    />
+                  )}
+                  name="password"
+                  rules={{required: 'Senha é obrigatória'}}
+                />
+                <Button title="Acessar" variant="primary" onPress={handleSubmit(handleSignIn)} />
               </Center>
               
               <Center mt={80}>
