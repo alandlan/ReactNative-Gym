a0d36622b21f1196a0380a45f5b637e772f3aa0e
alandlan <alanhorcrux@gmail.com>
2024-06-30 06:07:01 -0300

    refactor: Update ExerciseCard component to use API base URL for exercise thumbnail

--- gym-gluestack/src/components/ExerciseCard.tsx
+++ gym-gluestack/src/components/ExerciseCard.tsx
@@ -1,5 +1,6 @@
 import { ExerciseDTO } from "@dtos/ExerciseDTO";
 import { HStack,Heading,Icon,Image, Text, VStack } from "@gluestack-ui/themed";
+import { api } from "@services/api";
 import { ChevronRightIcon } from "lucide-react-native";
 import { TouchableOpacity,TouchableOpacityProps } from "react-native";
 
@@ -15,7 +16,7 @@ export function ExerciseCard({data,...rest}: Props){
             <HStack bg="$gray500" alignItems="center" p={8} pr={4} rounded={"$md"} mb={9}>
                 <Image 
                     alt="exercise image 1"
-                    source={{uri: "https://www.realsimple.com/thmb/dsrD3SKItujteW9JynEcaHDtA0g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/exercises-for-aches-and-pains-2000-3ba8f13cfd8d42c6ac9c0ec0d7fe3cea.jpg"}}
+                    source={{uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}`}}
                     w={64}
                     h={64}
                     rounded={"$md"}
